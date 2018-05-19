1ab9925bc6903646401573718ec7fc309b852132
Harvey Devereux <harveydevereux@googlemail.com>
2018-05-19 22:25:33 +0100

    init

---
+++ matrix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+while true;
+  do char=$((1+$RANDOM % 26))
---
+++ text_shortcuts.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# colors may not work in all terminals...
+
+# default
+DEFAULT="\e[39m\e[49m"
+
+# text colours
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+LIGHTGRAY="\e[37m"
+DARKGRAY="\e[90m"
+LIGHTRED="\e[91m"
+LIGTHGREEN="\e[92m"
+LIGTHYELLOW="\e[93m"
+LIGHTBLUE="\e[94m"
+LIGHTMAGENTA="\e[95m"
+LIGHTCYAN="\e[96m"
+WHITE="\e[97m"
+
+# backround colors
+
+BGBLACK="\e[40m"
+BGRED="\e[41m"
+BGGREEN="\e[42m"
+BGYELLOW="\e[43m"
+BGBLUE="\e[44m"
+BGMAGENTA="\e[45m"
+BGCYAN="\e[46m"
+BGLIGHTGRAY="\e[47m"
+BGDARKGRAY="\e[100m"
+BGLIGHTRED="\e[101m"
+BGLIGTHGREEN="\e[102m"
+BGLIGTHYELLOW="\e[103m"
+BGLIGHTBLUE="\e[104m"
+BGLIGHTMAGENTA="\e[105m"
+BGLIGHTCYAN="\e[106m"
+BGWHITE="\e[107m"
